cbcf6a36ef249eae36526694b0178b3b62a76b3d
ortima <account.net@.mail.ru>
2024-05-29 00:30:21 +0300

    init project

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+
+import clientRoutes from './routes/clientRoutes.js'
+import generalRoutes from './routes/generalRoutes.js'
+import managementRoutes from './routes/managementRoutes.js'
+import salesRoutes from './routes/salesRoutes.js'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+app.use('client', clientRoutes)
+app.use('general', generalRoutes)
+app.use('management', managementRoutes)
+app.use('sales', salesRoutes)
+
+const PORT = process.env.PORT || 5000
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
+  .catch(err => console.log(err))
